e4ba5891d93b4658338ee7e69010d42cb5accc9b
CodeZea1ot <codezealottutorials@gmail.com>
2019-10-04 16:56:18 -0400

    About + Contact Skeleton | CSS Updates

---
+++ page-about.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Template Name: About Page
+*/
+?>
+
+<?php get_header(); ?>
+<div class="hero">
+	
+</div>
+<main>
+	<section>
+		<h2 class="section-title">We’ve Been Providing Foam Solutions for 50 Years</h2>
+		<p>A division of <a class="link" href="http://www.barnhardt.net">Barnhardt Manufacturing Company</a>, NCFI was organized in 1964 by research chemist Dr. H. W. Bradley and Charlotte-based Barnhardt Manufacturing Company. NCFI quickly became a leader in the manufacturing of <a class="link" href="http://ncfi.com/flexible/products/">flexible polyurethane foam</a> for the bedding and furniture industries. In 1966, we moved into our present-day 46,000 square-foot plant in Mount Airy, NC. Since then, our facility has been expanded eight times to its current 280,000 square feet. NCFI has an additional North Carolina manufacturing plant in Hickory, and two more located in Dalton, GA and Salt Lake City, UT.</p>
+
+		<h2 class="section-title">We’ve Evolved to Meet the High Demand for Foam</h2>
+		<p>The company’s growth mirrors the demand for foam in a variety of industries, and for a growing number of applications. We offer a complete line of flexible foams for furniture seating, transportation seating, bedding, carpet underlay, and packaging. In Mount Airy we also innovate polyurethane foam solutions for spray foam insulation (commonly referred to as SPF), roofing, marine floatation, packaging, specialty molding, and many other uses. All of these lines are backed by our long-standing commitment to customer service and to staying on the leading edge of technology. We also sell and service application equipment for all of our foam systems.</p>
+	</section>
+</main>
+    
+
+<?php get_footer(); ?>
---
+++ page-contact.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Template Name: Contact Page
+*/
+?>
+
+<?php get_header(); ?>
+<div class="hero">
+	
+</div>
+<main>
+	
+</main>
+    
+
+<?php get_footer(); ?>
